dff6cd1fc51f7786e65771e1dbbf8e168237aa30
Ethan Baedke <ethanbaedke@gmail.com>
2024-11-22 20:10:17 -0700

    Initial commit

---
+++ premake5.lua
@@ -0,0 +1,29 @@
+workspace "Lumex"
+    architecture "x64"
+    cppdialect "C++17"
+
+    configurations { "Debug", "Release" }
+
+    filter "configurations:Debug"
+        targetdir "bin/debug"
+        objdir "bin-int/debug"
+
+        symbols "On"
+
+        defines { LX_DEBUG }
+
+    filter "configurations:Release"
+        targetdir "bin/release"
+        objdir "bin-int/release"
+
+        optimize "On"
+        buildoptions "/MT"
+
+        defines { LX_RELEASE }
+
+project "Engine"
+    location "Engine"
+    kind "ConsoleApp"
+    language "C++"
+    
+    files { "Engine/src/**.h", "Engine/src/**.cpp" }
