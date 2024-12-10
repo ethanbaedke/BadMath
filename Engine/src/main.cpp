b1c770a2a07f801ed222f36f1118ad185649be26
Ethan Baedke <ethanbaedke@gmail.com>
2024-12-09 20:12:46 -0700

    Removed memory management

--- Engine/src/main.cpp
+++ Engine/src/main.cpp
@@ -1,23 +1,13 @@
 #include <iostream>
 #include <string>
 
-#include "Core/MemoryManager.h"
-#include "Core/LXPtr.h"
-
-using namespace Lumex::Core;
-
 // Declare testing functions from the Testing folder
 extern void TEST_Mathematics();
-extern void TEST_Memory();
 
 int main()
 {
-	// Create managers
-	MemoryManager* renderManager = new MemoryManager();
-
 	// Testing
-	//TEST_Mathematics();
-	TEST_Memory();
+	TEST_Mathematics();
 
 	return 0;
 }
