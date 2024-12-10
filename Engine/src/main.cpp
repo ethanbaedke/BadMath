fe008d44823e8dfce1c0eee8b628f536f514118c
Ethan Baedke <ethanbaedke@gmail.com>
2024-12-09 21:31:38 -0700

    List

--- Engine/src/main.cpp
+++ Engine/src/main.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
 #include <string>
 
+#include "Collections/List.h"
+
 // Declare testing functions from the Testing folder
 extern void TEST_Mathematics();
+
+using namespace Lumex::Collections;
 
 int main()
 {
 	// Testing
-	TEST_Mathematics();
+	//TEST_Mathematics();
+
+	List<int> myList;
+	for (int i = 0; i < 10; i++)
+	{
+		myList.Add(i);
+	}
 
 	return 0;
 }
